ca71acf52207616ec74b7320b5c0a640051f854d
Mathew Hartley <matt@route66.sytes.net>
2013-11-14 12:23:08 +1300

    Add in international representation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,6 @@ australia.states.create!(:name => "Victoria")
 
 america = Country.create!(:name => "United States of America")
 america.states.create!(:name => "California")
+
+nz = Country.create!(:name => "New Zealand")
+nz.states.create!(:name => "Wellington")
